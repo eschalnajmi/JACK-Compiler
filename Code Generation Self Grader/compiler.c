a08d33d8f912e2a16b4f4333993be2485b52be46
Eschal Najmi <eschal.najmi@gmail.com>
2024-04-19 13:30:44 +0100

    code gen done - check on linux

--- Code Generation Self Grader/compiler.c
+++ Code Generation Self Grader/compiler.c
@@ -18,6 +18,7 @@ Date Work Commenced: 19/03/2024
 #include "compiler.h"
 #include <dirent.h>
 #include <stdio.h>
+#include <string.h>
 
 int InitCompiler ()
 {
@@ -34,16 +35,21 @@ ParserInfo compile (char* dir_name)
 
 	initglobalST();
 	while((enter2 = readdir(directory2)) != NULL){
-		//if (enter2->d_type != DT_REG) continue;
 
-		if (strcmp(enter2->d_name , ".") == 0 || strcmp(enter2->d_name ,"..") == 0) continue;
+		if (strcmp(enter2->d_name , ".") == 0 || strcmp(enter2->d_name ,"..") == 0 || 5 >= strlen(enter2->d_name) || strncmp(enter2->d_name + strlen(enter2->d_name) - 5, ".jack", 5) != 0) continue;
 
 		char full_path[1024];
 		snprintf(full_path, sizeof(full_path), "%s/%s", dir_name, enter2->d_name);
 
+		int filesize = strlen(enter2->d_name) + 3;
+        char* filepath = malloc(filesize);
+        strcpy(filepath, "./");
+        strcat(filepath, enter2->d_name);
+
 		InitParser(full_path);
 		globalParse();
 		StopParser();
+		free(filepath);
 	}
 
 	DIR *directory = opendir(dir_name);
@@ -52,15 +58,25 @@ ParserInfo compile (char* dir_name)
 
 	InitSymbolTable();
 	while((enter = readdir(directory)) != NULL){
-		if (strcmp(enter->d_name , ".") == 0 || strcmp(enter->d_name ,"..") == 0) continue;
+
+		
+
+		if (strcmp(enter->d_name , ".") == 0 || strcmp(enter->d_name ,"..") == 0 || 5 >= strlen(enter->d_name) || strncmp(enter->d_name + strlen(enter->d_name) - 5, ".jack", 5) != 0) continue;
+
 
 		char full_path[1024];
 		snprintf(full_path, sizeof(full_path), "%s/%s", dir_name, enter->d_name);
+
+		int fsize = strlen(dir_name) + strlen(enter->d_name) + 2;
+        char* fpath = malloc(fsize);
+        strcpy(fpath, "/");
+        strcat(fpath, enter->d_name);
 
 
 		InitParser(full_path);
 		p = Parse();
 		StopParser();
+		free(fpath);
 
 
 		if(p.er != none){
---
+++ Code Generation Self Grader/compiler.h
@@ -0,0 +1,51 @@
+#ifndef COMPILER_H
+#define COMPILER_H
+
+//#define TEST_COMPILER    // uncomment to run the compiler autograder
+
+#include "parser.h"
+#include "symbols.h"
+
+int InitCompiler ();
+ParserInfo compile (char* dir_name);
+int StopCompiler();
+
+FILE* vmFile;
+
+/*char* ErrorString (SyntaxErrors e)
+{
+
+	switch (e)
+	{
+		case none: return "no errors";
+		case lexerErr: return "lexer error";
+		case classExpected: return "keyword class expected";
+		case idExpected: return "identifier expected";
+		case openBraceExpected:	return "{ expected";
+		case closeBraceExpected: return "} expected";
+		case memberDeclarErr: return "class member declaration must begin with static, field, constructor , function , or method";
+		case classVarErr: return "class variables must begin with field or static";
+		case illegalType: return "a type must be int, char, boolean, or identifier";
+		case semicolonExpected: return "; expected";
+		case subroutineDeclarErr: return "subrouting declaration must begin with constructor, function, or method";
+		case openParenExpected: return "( expected";
+		case closeParenExpected: return ") expected";
+		case closeBracketExpected: return "] expected";
+		case equalExpected: return "= expected";
+		case syntaxError: return "syntax error";
+		// semantic errors
+		case undecIdentifier: return "undeclared identifier";
+		case redecIdentifier: return "redeclaration of identifier";
+		default: return "not a valid error code";
+	}
+}
+
+void PrintError (ParserInfo pn)
+{
+	if (pn.er == none)
+		printf ("No errors\n");
+	else
+		printf ("Error in file %s line %i at or near %s: %s\n" , pn.tk.fl , pn.tk.ln , pn.tk.lx , ErrorString(pn.er));
+}*/
+
+#endif
